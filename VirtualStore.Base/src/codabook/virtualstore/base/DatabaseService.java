07015fabb9df02e204a1a5cbcd6308744f4b7bd4
zhenjie911 <zhenjie911@gmail.com>
2017-05-02 18:52:57 +0800

    Add files via upload

---
+++ VirtualStore.Base/src/codabook/virtualstore/base/DatabaseService.java
@@ -0,0 +1,12 @@
+package codabook.virtualstore.base;
+
+
+
+public interface DatabaseService {
+	
+	public void registerUser();
+	public void addPromoCode();
+	public void updateUser();
+	public void loginUser();
+	
+}
